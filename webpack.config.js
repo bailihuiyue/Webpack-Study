7acd8a65b8658b5bf3ff0c9091d53e521547b236
Ones <33573384+bailihuiyue@users.noreply.github.com>
2019-06-10 22:03:16 +0800

    3-3

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     },
     module: {
         rules: [{
+            // lesson:3-2
             test: /\.(png|jpg|gif)$/,
             use: {
                 loader: 'url-loader',//url-loader也可以打包图片,但是会把图片变成base64写在页面中,优点,减少http请求,缺点,打包后的js文件特别大,会加载很久
@@ -17,6 +18,22 @@ module.exports = {
                     limit: 2048 //限制url-loader打包的文件大小,单位字节,小于limit的话直接写在js文件中,大于输出到文件夹中(仅url-loader有此设置)
                 }
             }
+        },
+        {
+            test: /\.(eot|ttf|svg)$/,
+            use: {
+                loader: 'file-loader'
+            }
+        },
+        {
+            test: /\.css$/,
+            // lesson:3-3
+            // css-loader 分析css文件之间的关系,最终合并成一段css
+            // style-loader 将css-loader生成的css代码挂在到html模板的head部分
+            // 数组loader的执行顺序:从右到左
+            // sass-loader先将sass编译成普通的css,然后再使用css-loader和style-loader
+            // postcss-loader在css3特性前加前缀(使用插件autoprefixer),例如-webkit-transform,详细配置见https://webpack.docschina.org/loaders/postcss-loader/
+            use: ['style-loader', 'css-loader','sass-loader','postcss-loader'] 
         }]
     },
     output: {
