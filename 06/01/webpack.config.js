773d7ae109033489ea17518ba1e7f895c29574a3
Ones <964981348@qq.com>
2019-06-27 17:49:57 +0800

    6-1(1)

---
+++ 06/01/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: {
+		main: './src/index.js'
+	},
+	resolveLoader: {
+		modules: ['node_modules', './loaders']
+	},
+	module: {
+		rules: [{
+			test: /\.js/,
+			use: [
+				{
+					loader: 'replaceLoader',
+				},
+				{
+					loader: 'replaceLoaderAsync',
+					options: {
+						name: 'lee'
+					}
+				},
+			]
+		}]
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].js'
+	}
+}
