882910336a8ce0fbc1c4cc8e11b9b8c269007a75
Ones <964981348@qq.com>
2019-07-04 14:09:32 +0800

    7-3,7-4

--- 07/03/my-project/vue.config.js
+++ 07/03/my-project/vue.config.js
@@ -1,7 +1,14 @@
+// lesson:7-3
+// vue-cli3没有方法可以暴露出原始的webpack配置文件,但是可以在根目录添加vue.config.js文件来扩展webpack配置
+// 使用时参考vue-cli官网,vue-cli已经对webpack进行了封装,所以要按照vue-cli的方式去写配置
+// https://cli.vuejs.org/zh/config/
 const path = require('path');
 
 module.exports = {
-	devServer: {
-		contentBase: [path.resolve(__dirname, 'static')],
+	// 一些特殊情况还是可以直接写webpack配置的
+	configureWebpack: {
+		devServer: {
+			contentBase: [path.resolve(__dirname, 'static')],
+		}
 	}
 }
